c4e9874ed4b2cd92fe210ac388e9ce497828eb28
logan crossan <crossanlogan@gmail.com>
2017-03-04 13:04:02 -0500

    user can provide a key to $.store()

--- jquery-localstorage.js
+++ jquery-localstorage.js
@@ -1,8 +1,13 @@
 (function ( $ ) {
-  $.store = function(obj) {
-    var key = Math.random().toString(36).substring(0, 15)
-    localStorage.setItem(key, JSON.stringify(obj))
-    return key
+  $.store = function(obj, key) {
+    if(key) {
+      localStorage.setItem(key, JSON.stringify(obj))
+      return [ obj, key ]
+    } else {
+      var key = Math.random().toString(36).substring(0, 15)
+      localStorage.setItem(key, JSON.stringify(obj))
+      return key
+    }
   }
 
   $.get = function(str) {
--- test/js/test.js
+++ test/js/test.js
@@ -7,6 +7,14 @@ QUnit.test('Inserting into localStorage', function(assert) {
   assert.ok(localStorage.getItem(key), 'Inserts into localStorage')
 })
 
+QUnit.test('Inserting into localStorage with a value', function(assert) {
+  var obj = { 'test': test }
+  var key = 'THIS IS A TEST'
+
+  $.store(obj, key)
+  assert.ok(localStorage.getItem(key), 'Inserts into localStorage')
+})
+
 QUnit.test('Getting from localStorage', function(assert) {
   localStorage.setItem('test-key', JSON.stringify('test-value'))
 
